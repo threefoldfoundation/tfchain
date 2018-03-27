ddbacf45d78811b485f196d87ef2ee2da7ab9d3d
Khaled Karam <khaledkbadr@outlook.com>
2018-03-27 14:02:03 +0200

    add autobuild
    Signed-off-by: Khaled Karam <khaledkbadr@outlook.com>

---
+++ autobuild/tfchain.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -ex
+
+apt-get update
+apt-get install git gcc wget -y
+
+# make output directory
+ARCHIVE=/tmp/archives
+FLIST=/tmp/flist
+mkdir -p $ARCHIVE
+
+# install go
+GOFILE=go1.10.linux-amd64.tar.gz
+wget https://dl.google.com/go/$GOFILE
+tar -C /usr/local -xzf $GOFILE
+mkdir -p /root/go
+export GOPATH=/root/go
+export PATH=$PATH:/usr/local/go/bin:$GOPATH/go/bin
+
+mkdir -p /root/go/src/github.com/threefoldfoundation
+cp -ar /tfchain /root/go/src/github.com/threefoldfoundation/tfchain
+
+TFCHAIN=$GOPATH/src/github.com/threefoldfoundation/tfchain
+TFCHAIND=$TFCHAIN/cmd/tfchaind
+TFCHAINC=$TFCHAIN/cmd/tfchainc
+
+pushd $TFCHAIND
+go build -ldflags "-linkmode external -s -w -extldflags -static" -o $FLIST/bin
+popd
+
+pushd $TFCHAINC
+go build -ldflags "-linkmode external -s -w -extldflags -static" -o $FLIST/bin
+popd
+
+# make sure binary is executable
+chmod +x $FLIST/bin/*
+
+tar -czf "/tmp/archives/tfchain.tar.gz" -C $FLIST .
