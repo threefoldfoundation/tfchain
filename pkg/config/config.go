6c8ad1f58e9947a8d9a5549011c73db673734f9b
Glen De Cauwsemaecker <decauwsemaecker.glen@gmail.com>
2018-05-06 16:55:47 +0200

    Update rivine vendor to latest master
    v1.0.5-alpha: timelocked outputs

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -49,6 +49,10 @@ func GetStandardnetGenesis() types.ChainConstants {
 	// use the threefold currency units
 	cfg.CurrencyUnits = GetCurrencyUnits()
 
+	// set transaction versions
+	cfg.DefaultTransactionVersion = types.TransactionVersionOne
+	cfg.GenesisTransactionVersion = types.TransactionVersionZero
+
 	// 2 minute block time
 	cfg.BlockFrequency = 120
 
@@ -80,8 +84,8 @@ func GetStandardnetGenesis() types.ChainConstants {
 
 	// Threefold Foundation receive all transactions fees in a single pool address,
 	// Block Creation Fees do well still go to the block creator creating the block.
-	cfg.TransactionFeeBeneficiary = unlockHashFromHex(
-		"017267221ef1947bb18506e390f1f9446b995acfb6d08d8e39508bb974d9830b8cb8fdca788e34")
+	cfg.TransactionFeeCondition = types.NewCondition(types.NewUnlockHashCondition(unlockHashFromHex(
+		"017267221ef1947bb18506e390f1f9446b995acfb6d08d8e39508bb974d9830b8cb8fdca788e34")))
 
 	// distribute initial coins
 	cfg.GenesisCoinDistribution = []types.CoinOutput{
@@ -89,49 +93,49 @@ func GetStandardnetGenesis() types.ChainConstants {
 			// 695M TFT for Capacity availability until 01/04
 			Value: cfg.CurrencyUnits.OneCoin.Mul64(695 * 1000 * 1000),
 			// temporary pool
-			UnlockHash: unlockHashFromHex("014ab1cf49a331bef9225a51a68623daf7e112fce0e81a91194a7f4fe7af1d9a793bc52d4676d0"),
+			Condition: types.NewCondition(types.NewUnlockHashCondition(unlockHashFromHex("014ab1cf49a331bef9225a51a68623daf7e112fce0e81a91194a7f4fe7af1d9a793bc52d4676d0"))),
 		},
 		{
 			// 3K TFT for dev/test purposes
 			Value: cfg.CurrencyUnits.OneCoin.Mul64(3000),
 			// @glendc
-			UnlockHash: unlockHashFromHex("01ad4f73417476f8b8350298681dd0fa8640baa53a91915417b1dd8103d118b543c992e6fba1c4"),
+			Condition: types.NewCondition(types.NewUnlockHashCondition(unlockHashFromHex("01ad4f73417476f8b8350298681dd0fa8640baa53a91915417b1dd8103d118b543c992e6fba1c4"))),
 		},
 		{
 			// 90K TFT for dev/test purposes
 			Value: cfg.CurrencyUnits.OneCoin.Mul64(90000),
 			// @foundation
-			UnlockHash: unlockHashFromHex("017eb744c7a97443d7927725bfb2a004384e4230386ea0f693f9ce1c1161d771878a1f048c887b"),
+			Condition: types.NewCondition(types.NewUnlockHashCondition(unlockHashFromHex("017eb744c7a97443d7927725bfb2a004384e4230386ea0f693f9ce1c1161d771878a1f048c887b"))),
 		},
 		{
 			// 1K TFT for dev/test purposes
 			Value: cfg.CurrencyUnits.OneCoin.Mul64(1000),
 			// @robvanmieghem
-			UnlockHash: unlockHashFromHex("01cc55df18eb3b86670deb6cfbb9b62b8463b62738426f0c14a7ae8926d6b556fbac3aab17f437"),
+			Condition: types.NewCondition(types.NewUnlockHashCondition(unlockHashFromHex("01cc55df18eb3b86670deb6cfbb9b62b8463b62738426f0c14a7ae8926d6b556fbac3aab17f437"))),
 		},
 		{
 			// 1K TFT for dev/test purposes
 			Value: cfg.CurrencyUnits.OneCoin.Mul64(1000),
 			// @RubenMattan
-			UnlockHash: unlockHashFromHex("01e2dc01a686fc0ca25612871a6515f2e3b804aa63244bf19449ecb3c9aaaf36f0cc6839b0af60"),
+			Condition: types.NewCondition(types.NewUnlockHashCondition(unlockHashFromHex("01e2dc01a686fc0ca25612871a6515f2e3b804aa63244bf19449ecb3c9aaaf36f0cc6839b0af60"))),
 		},
 		{
 			// 1K TFT for dev/test purposes
 			Value: cfg.CurrencyUnits.OneCoin.Mul64(1000),
 			// @FastGeert
-			UnlockHash: unlockHashFromHex("0166da8a4ab39a621637d9e7eb4e1fbaf95f905856af13af1268fc1a79c65b4f6686dec75c9d94"),
+			Condition: types.NewCondition(types.NewUnlockHashCondition(unlockHashFromHex("0166da8a4ab39a621637d9e7eb4e1fbaf95f905856af13af1268fc1a79c65b4f6686dec75c9d94"))),
 		},
 		{
 			// 1K TFT for dev/test purposes
 			Value: cfg.CurrencyUnits.OneCoin.Mul64(1000),
 			// @zaibon
-			UnlockHash: unlockHashFromHex("013dfb1c49e8b9a73bc8b460d9ef20fc1f40e0d034742950f70d983c455342719d1e9f656d002b"),
+			Condition: types.NewCondition(types.NewUnlockHashCondition(unlockHashFromHex("013dfb1c49e8b9a73bc8b460d9ef20fc1f40e0d034742950f70d983c455342719d1e9f656d002b"))),
 		},
 		{
 			// 2K TFT for dev/test purposes
 			Value: cfg.CurrencyUnits.OneCoin.Mul64(2000),
 			// @leesmet
-			UnlockHash: unlockHashFromHex("018a28615b277eb7e7a0e6921e85ad5b3ca378ac210b7f258b0b11ef313ea2ce98bd2e2510472d"),
+			Condition: types.NewCondition(types.NewUnlockHashCondition(unlockHashFromHex("018a28615b277eb7e7a0e6921e85ad5b3ca378ac210b7f258b0b11ef313ea2ce98bd2e2510472d"))),
 		},
 	}
 
@@ -141,19 +145,19 @@ func GetStandardnetGenesis() types.ChainConstants {
 			// 390 BS, initially allocated to a selected group of people (ambassadors)
 			Value: types.NewCurrency64(390),
 			// Pool address for other people (ambassadors)
-			UnlockHash: unlockHashFromHex("01746677df456546d93729066dd88514e2009930f3eebac3c93d43c88a108f8f9aa9e7c6f58893"),
+			Condition: types.NewCondition(types.NewUnlockHashCondition(unlockHashFromHex("01746677df456546d93729066dd88514e2009930f3eebac3c93d43c88a108f8f9aa9e7c6f58893"))),
 		},
 		{
 			// 100 BS, one BS for each first-generation TFT node
 			Value: types.NewCurrency64(100),
 			// @glendc (temporary)
-			UnlockHash: unlockHashFromHex("01ad4f73417476f8b8350298681dd0fa8640baa53a91915417b1dd8103d118b543c992e6fba1c4"),
+			Condition: types.NewCondition(types.NewUnlockHashCondition(unlockHashFromHex("01ad4f73417476f8b8350298681dd0fa8640baa53a91915417b1dd8103d118b543c992e6fba1c4"))),
 		},
 		{
 			// 10 BS, for dev/validation/test purposes
 			Value: types.NewCurrency64(10),
 			// @foundation @robvanmieghem
-			UnlockHash: unlockHashFromHex("01cc55df18eb3b86670deb6cfbb9b62b8463b62738426f0c14a7ae8926d6b556fbac3aab17f437"),
+			Condition: types.NewCondition(types.NewUnlockHashCondition(unlockHashFromHex("01cc55df18eb3b86670deb6cfbb9b62b8463b62738426f0c14a7ae8926d6b556fbac3aab17f437"))),
 		},
 	}
 
@@ -166,6 +170,10 @@ func GetTestnetGenesis() types.ChainConstants {
 
 	// use the threefold currency units
 	cfg.CurrencyUnits = GetCurrencyUnits()
+
+	// set transaction versions
+	cfg.DefaultTransactionVersion = types.TransactionVersionOne
+	cfg.GenesisTransactionVersion = types.TransactionVersionZero
 
 	// 2 minute block time
 	cfg.BlockFrequency = 120
@@ -202,7 +210,7 @@ func GetTestnetGenesis() types.ChainConstants {
 			// Create 100M coins
 			Value: cfg.CurrencyUnits.OneCoin.Mul64(100 * 1000 * 1000),
 			// @leesmet
-			UnlockHash: unlockHashFromHex("01fc8714235d549f890f35e52d745b9eeeee34926f96c4b9ef1689832f338d9349b453898f7e51"),
+			Condition: types.NewCondition(types.NewUnlockHashCondition(unlockHashFromHex("01fc8714235d549f890f35e52d745b9eeeee34926f96c4b9ef1689832f338d9349b453898f7e51"))),
 		},
 	}
 
@@ -212,7 +220,7 @@ func GetTestnetGenesis() types.ChainConstants {
 			// Create 3K blockstakes
 			Value: types.NewCurrency64(3000),
 			// @leesmet
-			UnlockHash: unlockHashFromHex("01fc8714235d549f890f35e52d745b9eeeee34926f96c4b9ef1689832f338d9349b453898f7e51"),
+			Condition: types.NewCondition(types.NewUnlockHashCondition(unlockHashFromHex("01fc8714235d549f890f35e52d745b9eeeee34926f96c4b9ef1689832f338d9349b453898f7e51"))),
 		},
 	}
 
@@ -225,6 +233,11 @@ func GetDevnetGenesis() types.ChainConstants {
 
 	// use the threefold currency units
 	cfg.CurrencyUnits = GetCurrencyUnits()
+
+	// set transaction versions
+	cfg.DefaultTransactionVersion = types.TransactionVersionOne
+	// no need to keep v0 as genesis transaction version for the dev network
+	cfg.GenesisTransactionVersion = types.TransactionVersionOne
 
 	// 12 seconds, slow enough for developers to see
 	// ~each block, fast enough that blocks don't waste time
@@ -266,7 +279,7 @@ func GetDevnetGenesis() types.ChainConstants {
 			Value: cfg.CurrencyUnits.OneCoin.Mul64(100 * 1000 * 1000),
 			// belong to wallet with mnemonic:
 			// carbon boss inject cover mountain fetch fiber fit tornado cloth wing dinosaur proof joy intact fabric thumb rebel borrow poet chair network expire else
-			UnlockHash: unlockHashFromHex("015a080a9259b9d4aaa550e2156f49b1a79a64c7ea463d810d4493e8242e6791584fbdac553e6f"),
+			Condition: types.NewCondition(types.NewUnlockHashCondition(unlockHashFromHex("015a080a9259b9d4aaa550e2156f49b1a79a64c7ea463d810d4493e8242e6791584fbdac553e6f"))),
 		},
 	}
 
@@ -277,7 +290,7 @@ func GetDevnetGenesis() types.ChainConstants {
 			Value: types.NewCurrency64(3000),
 			// belongs to wallet with mnemonic:
 			// carbon boss inject cover mountain fetch fiber fit tornado cloth wing dinosaur proof joy intact fabric thumb rebel borrow poet chair network expire else
-			UnlockHash: unlockHashFromHex("015a080a9259b9d4aaa550e2156f49b1a79a64c7ea463d810d4493e8242e6791584fbdac553e6f"),
+			Condition: types.NewCondition(types.NewUnlockHashCondition(unlockHashFromHex("015a080a9259b9d4aaa550e2156f49b1a79a64c7ea463d810d4493e8242e6791584fbdac553e6f"))),
 		},
 	}
 
