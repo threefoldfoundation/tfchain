eddadf283b0985962b8de73d4afa63fc3b517597
Glen De Cauwsemaecker <decauwsemaecker.glen@gmail.com>
2018-06-02 19:42:28 +0200

    ensure multisig cannot be used on standardnet before block 42000

--- cmd/tfchaind/main.go
+++ cmd/tfchaind/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/threefoldfoundation/tfchain/pkg/config"
 
 	"github.com/rivine/rivine/pkg/daemon"
+	"github.com/rivine/rivine/types"
 )
 
 var (
@@ -26,6 +27,12 @@ func main() {
 
 // SetupNetworks injects the correct chain constants and genesis nodes based on the chosen network
 func SetupNetworks(name string) (daemon.NetworkConfig, error) {
+	// configure any tfchain-specific logic related to how
+	// unlock conditions/fulfillments are to be interpreted/used.
+	ConfigureUnlockConditions(name)
+
+	// return the network configuration, based on the network name,
+	// which includes the genesis block as well as the bootstrap peers
 	switch name {
 	case standardnet:
 		return daemon.NetworkConfig{
@@ -46,4 +53,54 @@ func SetupNetworks(name string) (daemon.NetworkConfig, error) {
 	default:
 		return daemon.NetworkConfig{}, fmt.Errorf("Netork name %q not recognized", name)
 	}
+}
+
+// ConfigureUnlockConditions configures the unlock conditions,
+// as to define the unlock conditions and fulfillments.
+// For the most parts the configuration follows the standard Rivine unlock condition configuration,
+// but there are a couple of differences:
+//
+// * if the networkName equals "standard",
+//   the MultiSignatureCondition is only considered standard since block height 42000,
+//   giving peers on the standard network more or less 7 days, since 1.0.6 was released
+func ConfigureUnlockConditions(networkName string) {
+	if networkName != standardnet {
+		return // only apply the blockheight-based restrictions for standard net
+	}
+	// Overwrite the Rivine-standard MultiSignatureCondition with our wrapped version,
+	// as to ensure that this condition is only introduced in the
+	// standard tfchain since blockheight 42000
+	overwriteMultiSignatureConditionType()
+	// MultiSignatureFulfillment doesn't need to be overriden,
+	// as it is impossible to use it as long as there isn't an unspend output
+	// which uses the MultiSignatureCondition
+}
+
+func overwriteMultiSignatureConditionType() {
+	types.RegisterUnlockConditionType(types.ConditionTypeMultiSignature,
+		func() types.MarshalableUnlockCondition { return new(MultiSignatureCondition) })
+}
+
+// MultiSignatureCondition wraps around the Rivine-standard MultiSignatureCondition type,
+// as to ensure that in the standard network of tfchain, it can only be used since blockheight 42000
+type MultiSignatureCondition struct {
+	*types.MultiSignatureCondition
+}
+
+const (
+	// MinimumBlockHeightForMultiSignatureConditions defines the blockheight
+	// since when MultiSignatureConditions are considered standard on the tfchain standard network.
+	MinimumBlockHeightForMultiSignatureConditions = types.BlockHeight(42000)
+)
+
+// IsStandardCondition implements UnlockCondition.IsStandardCondition,
+// wrapping around the internal MultiSignatureCondition's IsStandardCondition check,
+// adding a pre-check of the blockheight
+func (msc MultiSignatureCondition) IsStandardCondition(ctx types.StandardCheckContext) error {
+	if ctx.BlockHeight < MinimumBlockHeightForMultiSignatureConditions {
+		return fmt.Errorf(
+			"multisignature conditions are only allowed since blockheight %d",
+			MinimumBlockHeightForMultiSignatureConditions)
+	}
+	return msc.MultiSignatureCondition.IsStandardCondition(ctx)
 }
---
+++ cmd/tfchaind/multisig_standard_test.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/rivine/rivine/types"
+)
+
+func TestMultiSignatureConditionIsStandardCondition(t *testing.T) {
+	ctx := types.StandardCheckContext{}
+	// create the condition manually
+	msc := MultiSignatureCondition{
+		MultiSignatureCondition: &types.MultiSignatureCondition{
+			UnlockHashes: []types.UnlockHash{
+				unlockHashFromHex("01746677df456546d93729066dd88514e2009930f3eebac3c93d43c88a108f8f9aa9e7c6f58893"),
+				unlockHashFromHex("01c46a8e1e7f1bb0e3b7ec6c93b9c4f3e5d89e855f5a57f22d478d72d6233391153fac7d179087"),
+			},
+			MinimumSignatureCount: 1,
+		},
+	}
+	// ensure that the internal condition's standard check does pass
+	err := msc.MultiSignatureCondition.IsStandardCondition(ctx)
+	if err != nil {
+		t.Fatal("expected standard condition check pass, but it failed: ", err)
+	}
+	// ensure that our internal condition's standard check fails
+	err = msc.IsStandardCondition(ctx)
+	if err == nil {
+		t.Fatal("expected standard condition check to fail, but it didn't")
+	}
+}
+
+func TestRegisteredMultiSignatureCondition(t *testing.T) {
+	// temporary overwrite multisig condition type, just for this unit test
+	overwriteMultiSignatureConditionType()
+	defer types.RegisterUnlockConditionType(types.ConditionTypeMultiSignature,
+		func() types.MarshalableUnlockCondition { return new(types.MultiSignatureCondition) })
+
+	const jsonCondition = `{
+	"type": 4,
+	"data": {
+		"unlockhashes": [
+			"01746677df456546d93729066dd88514e2009930f3eebac3c93d43c88a108f8f9aa9e7c6f58893",
+			"01c46a8e1e7f1bb0e3b7ec6c93b9c4f3e5d89e855f5a57f22d478d72d6233391153fac7d179087"
+		],
+		"minimumsignaturecount": 2
+	}
+}`
+
+	// decode our json-encoded multisig condition
+	var condition types.UnlockConditionProxy
+	err := condition.UnmarshalJSON([]byte(jsonCondition))
+	if err != nil {
+		t.Fatal("failed to decode multisignature condition into proxy condition: ", err)
+	}
+
+	// ensure the condition type is MultiSig
+	if ct := condition.ConditionType(); ct != types.ConditionTypeMultiSignature {
+		t.Fatalf("expected condition type to be %d, but it was %d instead",
+			types.ConditionTypeMultiSignature, ct)
+	}
+
+	// sanity check, ensure it is our type
+	if _, ok := condition.Condition.(*MultiSignatureCondition); !ok {
+		t.Fatalf("expected condition type to be (our) *MultiSignatureCondition, but it was %T instead",
+			condition.Condition)
+	}
+
+	// ensure that it can't be used yet at height 0
+	ctx := types.StandardCheckContext{}
+	err = condition.IsStandardCondition(ctx)
+	if err == nil {
+		t.Fatal("expected standard condition check to fail, but it didn't")
+	}
+
+	// ensure that it can be used at the minimum height
+	ctx.BlockHeight = MinimumBlockHeightForMultiSignatureConditions
+	err = condition.IsStandardCondition(ctx)
+	if err != nil {
+		t.Fatal("expected standard condition check pass, but it failed: ", err)
+	}
+}
+
+func unlockHashFromHex(hstr string) (uh types.UnlockHash) {
+	err := uh.LoadString(hstr)
+	if err != nil {
+		panic(fmt.Sprintf("func unlockHashFromHex(%s) failed: %v", hstr, err))
+	}
+	return
+}
