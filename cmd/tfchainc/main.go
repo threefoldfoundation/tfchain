6802fdec5976cf97255453aa2442ff266aadb8ce
Glen De Cauwsemaecker <decauwsemaecker.glen@gmail.com>
2018-06-14 18:11:55 +0200

    sync with latest rivine master
    CLI can now work with explorer configs as well,
    and CLI can even default to a default/standardnet config should this be required,
    daemon constants are also only fetched if needed, and not a second before

--- cmd/tfchainc/main.go
+++ cmd/tfchainc/main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"github.com/rivine/rivine/modules"
 	"github.com/rivine/rivine/pkg/client"
 	"github.com/threefoldfoundation/tfchain/pkg/config"
 )
 
 func main() {
 	bchainInfo := config.GetBlockchainInfo()
-	client.DefaultCLIClient("", bchainInfo.Name, func(cfg client.Config) client.Config {
+	client.DefaultCLIClient("", bchainInfo.Name, func(icfg *client.Config) client.Config {
+		cfg := daemonOrDefaultConfig(icfg)
 		switch cfg.NetworkName {
 		case config.NetworkNameStandard:
 			// overwrite standard network genesis block stamp,
@@ -20,4 +22,17 @@ func main() {
 		}
 		return cfg
 	})
+}
+
+// daemonOrDefaultConfig uses a default config
+// if a config was not returned originating from the used daemon's constants.
+func daemonOrDefaultConfig(icfg *client.Config) client.Config {
+	if icfg != nil {
+		return *icfg
+	}
+
+	bchainInfo := config.GetBlockchainInfo()
+	chainConstants := config.GetStandardnetGenesis()
+	daemonConstants := modules.NewDaemonConstants(bchainInfo, chainConstants)
+	return client.ConfigFromDaemonConstants(daemonConstants)
 }
