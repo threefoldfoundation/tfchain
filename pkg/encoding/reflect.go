b823358d784eb6544813d56ce9ef70e8c18f1bd8
Glen De Cauwsemaecker <contact@glendc.com>
2018-10-16 21:45:44 +0200

    add comment to clarify for loop in isFieldHidden

--- pkg/encoding/reflect.go
+++ pkg/encoding/reflect.go
@@ -11,6 +11,9 @@ func isFieldHidden(val reflect.Value, index int) bool {
 		return true
 	}
 	for _, r := range field.Name {
+		// yes, we do want to return always,
+		// we only have a for loop as it seems to be the
+		// easiest and most efficient way to do so in Go
 		return r == '_' || unicode.IsLower(r)
 	}
 	return true
