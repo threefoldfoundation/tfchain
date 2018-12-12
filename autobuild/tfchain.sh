6a07559440a55da32d4b00faffeeb3ba8daf8c69
Ahmed T. Youssef <xmonader@gmail.com>
2018-12-12 12:47:04 +0200

    add example for zos

--- autobuild/tfchain.sh
+++ autobuild/tfchain.sh
@@ -8,6 +8,7 @@ apt-get install git gcc wget -y
 ARCHIVE=/tmp/archives
 FLIST=/tmp/flist
 mkdir -p $ARCHIVE
+mkdir -p $FLIST/bin
 
 # install go
 GOFILE=go1.10.linux-amd64.tar.gz
@@ -26,17 +27,19 @@ TFCHAINC=$TFCHAIN/cmd/tfchainc
 BRIDGED=$TFCHAIN/cmd/bridged
 
 pushd $TFCHAIND
-go build -ldflags "-linkmode external -s -w -extldflags -static" -o $FLIST/bin
+# go build -ldflags "-linkmode external -s -w -extldflags -static" -o $FLIST/bin
+go build -o $FLIST/bin/tfchaind
 popd
 
 pushd $TFCHAINC
-go build -ldflags "-linkmode external -s -w -extldflags -static" -o $FLIST/bin
+# go build -ldflags "-linkmode external -s -w -extldflags -static" -o $FLIST/bin
+go build -o $FLIST/bin/tfchainc
 popd
 
 pushd $BRIDGED
-go build -ldflags "-linkmode external -s -w -extldflags -static" -o $FLIST/bin
+# go build -ldflags "-linkmode external -s -w -extldflags -static" -o $FLIST/bin
+go build -o $FLIST/bin/bridged
 popd
-
 # make sure binary is executable
 chmod +x $FLIST/bin/*
 
