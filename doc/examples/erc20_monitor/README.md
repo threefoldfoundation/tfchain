9bd2e33e55fd99709ffcdf5cd34e6c1c54b8c626
LeeSmet <lee.smet@hotmail.com>
2019-01-22 17:53:28 +0100

    Close #287: Rework demo and add withdraw function

--- doc/examples/erc20_monitor/README.md
+++ doc/examples/erc20_monitor/README.md
@@ -1,27 +1,19 @@
 # erc20 monitor
 
-This is a demo application showing the erc20 (T)TFT to regular (T)TFT conversion. Right now the page
-is only able to show the balance of an address.
-
-## Contract
-
-A test contract has been deployed on the [Rinkeby testnet](https://rinkeby.etherscan.io/). The contract source can
-be found in [testcontract.sol](testcontract.sol).
+This is a demo application showing the erc20 (T)TFT to regular (T)TFT conversion. The application leverages the light
+client code used by the bridge and tfchain daemon eth validator modules. The demo application itself is only connected
+to the ethereum network
 
 ## Running
 
 If you want to run this example yourself, simply build the example (`go build`), and run the produced binary
-in this directory. You will require an [etherscan](https://etherscan.io) API key. You also need to specify the
-listening address for the server.
+in this directory. You will need to pass at least the `--ethnetwork` flag to specify the ethereum network to use.
+The only 2 allowed options are `rinkeby` and `ropsten`. By default the demo will use the contract addresses defined
+by tfchain. If you are running a development setup, you will likely need to deploy your own contract. The address
+of this contract can then be passed with the `--contract-address` flag. For a list of all supported flags, pass
+the `--help` flag.
 
-The following example runs the server on `port 8888` on every interface on the local machine, with an `API Key`
-set as an environment variable.
+## Withdrawing
 
-```bash
-./erc20_monitor $APIKEY :8888
-```
-
-## Development
-
-The webpage uses [vue.js](https://vuejs.org). Local development requires `vue cli` for some necessary tools.
-Further explanation on the web development process can be found on the official vue docs.
+In order to withdraw from the demo, it's account will need to be funded with both ethers and tokens. The demo's
+address to send to is displayed on the web page.
--- doc/examples/erc20_monitor/main.go
+++ doc/examples/erc20_monitor/main.go
@@ -2,76 +2,274 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"math/big"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 
-	log "github.com/Sirupsen/logrus"
-
-	"github.com/nanmu42/etherscan-api"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/log"
+	"github.com/spf13/cobra"
+	"github.com/threefoldfoundation/tfchain/pkg/eth/erc20"
 )
 
-var (
-	// client is thread safe
-	client *etherscan.Client
+const (
+	precision = 1000000000.
 )
 
-func main() {
-	if len(os.Args) != 3 {
-		log.Fatalf("Wrong number of arguments.\nUsage:\n\t%s [apikey] [server address]", os.Args[0])
-	}
-	apiKey := os.Args[1]
-	serverAddr := os.Args[2]
+// Commands defines the CLI Commands for the tfchain client.
+type Commands struct {
+	EthNetworkName string
 
-	client = etherscan.New(etherscan.Rinkby, apiKey)
+	// eth port for light client
+	EthPort uint16
 
-	server := setupServer(serverAddr)
-	if err := server.ListenAndServe(); err != nil {
-		log.Fatal("Error while serving requests:", err)
-	}
+	// eth bootnodes
+	EthBootNodes []string
+
+	// eth account flags
+	accJSON string
+	accPass string
+
+	EthLog          int
+	ContractAddress string
+
+	RootPersistentDir string
+
+	// port to make the server listen on
+	ServerAddr uint16
 }
 
-func setupServer(serverAddr string) *http.Server {
-	server := http.Server{Addr: serverAddr}
-
-	mux := http.NewServeMux()
-	mux.HandleFunc("/tokenbalance", getBalance)
-	mux.Handle("/", http.FileServer(http.Dir("./public")))
-
-	server.Handler = mux
-
-	return &server
+type demoExchange struct {
+	contract *erc20.BridgeContract
 }
 
-// sets Cors header for development
-func enableCors(w *http.ResponseWriter) {
-	(*w).Header().Set("Access-Control-Allow-Origin", "*")
-}
-
-// getBalance is the handler for GET /tokenbalance. This endpoint
-// returns the token balance on the given address.
-func getBalance(w http.ResponseWriter, r *http.Request) {
-	enableCors(&w)
-	if r.Method != "GET" {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		return
-	}
-
-	q := r.URL.Query()
-	contractAddres := q.Get("contractaddress")
-	address := q.Get("address")
-	if address == "" || contractAddres == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	balance, err := client.TokenBalance(contractAddres, address)
+// GetAddress returns the loaded address of the demo exchange
+func (de *demoExchange) GetAddress(w http.ResponseWriter, r *http.Request) {
+	addr, err := de.contract.AccountAddress()
 	if err != nil {
-		log.Error("Unable to get token balance:", err)
-		// TODO: Figure out the right error code for this case
 		w.WriteHeader(http.StatusInternalServerError)
+		res := struct {
+			Error error `json:"error"`
+		}{
+			Error: err,
+		}
+		json.NewEncoder(w).Encode(res)
+		return
+	}
+
+	res := struct {
+		Address common.Address `json:"address"`
+	}{
+		Address: addr,
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(res)
+}
+
+func (de *demoExchange) GetBalance(w http.ResponseWriter, r *http.Request) {
+	balance, err := de.contract.EthBalance()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		res := struct {
+			Error error `json:"error"`
+		}{
+			Error: err,
+		}
+		json.NewEncoder(w).Encode(res)
+		return
+	}
+
+	res := struct {
+		Balance *big.Int `json:"balance"`
+	}{
+		Balance: balance,
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(res)
+}
+
+func (de *demoExchange) GetTokenBalance(w http.ResponseWriter, r *http.Request) {
+	addr, err := de.contract.AccountAddress()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		res := struct {
+			Error error `json:"error"`
+		}{
+			Error: err,
+		}
+		json.NewEncoder(w).Encode(res)
+		return
+	}
+
+	balance, err := de.contract.TokenBalance(addr)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		res := struct {
+			Error error `json:"error"`
+		}{
+			Error: err,
+		}
+		json.NewEncoder(w).Encode(res)
+		return
+	}
+
+	res := struct {
+		Balance *big.Int `json:"balance"`
+	}{
+		Balance: balance,
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(res)
+}
+
+func (de *demoExchange) Withdraw(w http.ResponseWriter, r *http.Request) {
+	body := struct {
+		Address common.Address `json:"address"`
+		Amount  *big.Int       `json:"amount"`
+	}{}
+
+	err := json.NewDecoder(r.Body).Decode(&body)
+	if err != nil {
+		log.Info("Failed to decode withdraw body", "err", err)
+		w.WriteHeader(http.StatusBadRequest)
+
+		res := struct {
+			Error error `json:"error"`
+		}{
+			Error: err,
+		}
+		json.NewEncoder(w).Encode(res)
+		return
+	}
+
+	fmt.Println(body)
+
+	err = de.contract.TransferFunds(body.Address, body.Amount)
+	if err != nil {
+		log.Info("Failed to transfer tokens", "err", err)
+		w.WriteHeader(http.StatusBadRequest)
+		res := struct {
+			Error error `json:"error"`
+		}{
+			Error: err,
+		}
+		json.NewEncoder(w).Encode(res)
 		return
 	}
 
 	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(balance)
+}
+
+// Root represents the root command,
+func (cmd *Commands) Root(_ *cobra.Command, args []string) error {
+	log.Root().SetHandler(log.LvlFilterHandler(log.Lvl(cmd.EthLog), log.StreamHandler(os.Stderr, log.TerminalFormat(true))))
+
+	closeChan := make(chan struct{})
+
+	contract, err := erc20.NewBridgeContract(strings.ToLower(cmd.EthNetworkName), cmd.EthBootNodes, cmd.ContractAddress, int(cmd.EthPort), cmd.accJSON, cmd.accPass, cmd.RootPersistentDir, closeChan)
+	if err != nil {
+		log.Error("Failed to create contract bindings", "err", err)
+		return err
+	}
+
+	de := &demoExchange{contract: contract}
+
+	server := http.Server{Addr: ":" + strconv.Itoa(int(cmd.ServerAddr))}
+	mux := http.NewServeMux()
+	mux.HandleFunc("/balance", de.GetBalance)
+	mux.HandleFunc("/address", de.GetAddress)
+	mux.HandleFunc("/tokenbalance", de.GetTokenBalance)
+	mux.HandleFunc("/withdraw", de.Withdraw)
+
+	mux.Handle("/", http.FileServer(http.Dir("./rework/public")))
+
+	server.Handler = mux
+
+	if err := server.ListenAndServe(); err != nil {
+		log.Error("Server error", "err", err)
+		return err
+	}
+
+	return nil
+}
+
+func main() {
+	cmd := new(Commands)
+
+	// define commands
+	cmdRoot := &cobra.Command{
+		Use:          "demo_exchange",
+		Short:        "start the demo_exchange",
+		Long:         `start the demo_exchange`,
+		Args:         cobra.ExactArgs(0),
+		SilenceUsage: true,
+		RunE:         cmd.Root,
+	}
+
+	// define flags
+	cmdRoot.Flags().StringVarP(
+		&cmd.RootPersistentDir,
+		"persistent-directory", "d",
+		"demo_exchange",
+		"location of the root directory used to store persistent data",
+	)
+
+	// bridge flags
+	cmdRoot.Flags().StringVar(
+		&cmd.EthNetworkName,
+		"ethnetwork", "",
+		"The ethereum network, {rinkeby, ropsten}, defaults to the TFT-linked network",
+	)
+	cmdRoot.Flags().Uint16Var(
+		&cmd.EthPort,
+		"ethport", 30303,
+		"port for the ethereum deamon",
+	)
+	cmdRoot.Flags().StringSliceVar(
+		&cmd.EthBootNodes,
+		"ethbootnodes", nil,
+		"Override the default ethereum bootnodes, a comma seperated list of enode URLs (enode://pubkey1@ip1:port1)",
+	)
+
+	// bridge account
+	cmdRoot.Flags().StringVar(
+		&cmd.accJSON,
+		"account-json", "",
+		"the path to an account file. If set, the specified account will be loaded",
+	)
+
+	cmdRoot.Flags().StringVar(
+		&cmd.accPass,
+		"account-password", "",
+		"Password for the bridge account",
+	)
+
+	cmdRoot.Flags().IntVarP(
+		&cmd.EthLog,
+		"ethereum-log-lvl", "e", 3,
+		"Log lvl for the ethereum logger",
+	)
+
+	cmdRoot.Flags().StringVar(
+		&cmd.ContractAddress,
+		"contract-address", "",
+		"Use a custom contract",
+	)
+
+	cmdRoot.Flags().Uint16Var(
+		&cmd.ServerAddr,
+		"port", 8080,
+		"Port to serve the demo web page on",
+	)
+
+	// execute logic
+	if err := cmdRoot.Execute(); err != nil {
+		os.Exit(1)
+	}
 }
