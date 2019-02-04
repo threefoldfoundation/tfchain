4aeac8fa2d5a759c98989f845b64516426cd6a3d
LeeSmet <lee.smet@hotmail.com>
2019-02-04 14:44:48 +0100

    Restructure the cli command tree to improve stability

--- cmd/light-client/main.go
+++ cmd/light-client/main.go
@@ -63,60 +63,56 @@ without having to download the entire blockchain.`,
 		return
 	}
 
-	walletCmds := []*cobra.Command{}
-
 	for _, walletName := range walletNames {
-		walletCmds = append(walletCmds, &cobra.Command{
+		walletCmd := &cobra.Command{
 			Use:   walletName,
 			Short: fmt.Sprintf("Get the balance of wallet %s", walletName),
 			Long: fmt.Sprintf(`Get the balance of wallet %v.
 Additional actions can be performed on this wallet via subcommands`, walletName),
 			RunE: cmd.walletBalance,
 			Args: cobra.NoArgs,
-		})
-	}
+		}
 
-	rootCmd.AddCommand(walletCmds...)
+		rootCmd.AddCommand(walletCmd)
 
-	seedCmd := &cobra.Command{
-		Use:   "seed",
-		Short: "Print the seed of this wallet",
-		Long: `Print the seed of this wallet as a mnemonic. This mnemonic can be stored and later used to recover
-the wallet`,
-		RunE: cmd.walletSeed,
-	}
+		seedCmd := &cobra.Command{
+			Use:   "seed",
+			Short: "Print the seed of this wallet",
+			Long: `Print the seed of this wallet as a mnemonic. This mnemonic can be stored and later used to recover
+	the wallet`,
+			RunE: cmd.walletSeed,
+		}
 
-	txCmd := &cobra.Command{
-		Use:   "send <address> <amount>",
-		Short: "Send coins using a transaction",
-		Long: `Create a new transaction to send the specified amount of coins to the specified address.
-Inputs are selected automatically from the available ones. The transactionfee is set to the lowest permitted value.
-In case the sum of the inputs warants a refund output, that is also added automatically as well.`,
-		RunE: cmd.walletSend,
-		Args: cobra.ExactArgs(2),
-	}
-	txCmd.Flags().BoolVar(&cmd.GenerateNewRefundAddress, "new-refund-addr", false, "Generate a new refund address instead of reusing an existing address")
-	txCmd.Flags().StringVarP(&cmd.DataString, "data", "d", "", "Attach this string as arbitrary data to the transaction")
+		txCmd := &cobra.Command{
+			Use:   "send <address> <amount>",
+			Short: "Send coins using a transaction",
+			Long: `Create a new transaction to send the specified amount of coins to the specified address.
+	Inputs are selected automatically from the available ones. The transactionfee is set to the lowest permitted value.
+	In case the sum of the inputs warants a refund output, that is also added automatically as well.`,
+			RunE: cmd.walletSend,
+			Args: cobra.ExactArgs(2),
+		}
+		txCmd.Flags().BoolVar(&cmd.GenerateNewRefundAddress, "new-refund-addr", false, "Generate a new refund address instead of reusing an existing address")
+		txCmd.Flags().StringVarP(&cmd.DataString, "data", "d", "", "Attach this string as arbitrary data to the transaction")
 
-	addressesCmd := &cobra.Command{
-		Use:   "addresses",
-		Short: "List all loaded addresses",
-		Long: `List all loaded addresses. If an address owned by this wallet is not in the list after recovering,
-you can load more addresses using the 'load' sub command`,
-		RunE: cmd.walletAddresses,
-		Args: cobra.NoArgs,
-	}
+		addressesCmd := &cobra.Command{
+			Use:   "addresses",
+			Short: "List all loaded addresses",
+			Long: `List all loaded addresses. If an address owned by this wallet is not in the list after recovering,
+	you can load more addresses using the 'load' sub command`,
+			RunE: cmd.walletAddresses,
+			Args: cobra.NoArgs,
+		}
 
-	loadCmd := &cobra.Command{
-		Use:   "load",
-		Short: "Generate additional keys",
-		Long: `Generate additional keys. The index used to generate the keys is continued from the allready loaded keys. After loading the wallet
-persistent data is updated to reflect the additional keys, and they will be available for future use.`,
-		RunE: cmd.walletLoad,
-		Args: cobra.ExactArgs(1),
-	}
+		loadCmd := &cobra.Command{
+			Use:   "load <amount>",
+			Short: "Generate additional keys",
+			Long: `Generate additional keys. The index used to generate the keys is continued from the allready loaded keys. After loading the wallet
+	persistent data is updated to reflect the additional keys, and they will be available for future use.`,
+			RunE: cmd.walletLoad,
+			Args: cobra.ExactArgs(1),
+		}
 
-	for _, walletCmd := range walletCmds {
 		walletCmd.AddCommand(seedCmd, txCmd, addressesCmd, loadCmd)
 	}
 
