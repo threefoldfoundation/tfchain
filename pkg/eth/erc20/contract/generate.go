8433eb5bbc96c4531c6949101316d9cef414bc64
LeeSmet <lee.smet@hotmail.com>
2019-04-29 15:33:11 +0200

    Add contract version to generated bindings

--- pkg/eth/erc20/contract/generate.go
+++ pkg/eth/erc20/contract/generate.go
@@ -1,4 +1,4 @@
 package contract
 
 // The abigen tool can be installed using `go install github.com/ethereum/go-ethereum/cmd/abigen`
-//go:generate abigen --sol ../../../../erc20/contract/tokenV1.sol --pkg contract --out contract.go
+//go:generate abigen --sol ../../../../erc20/contract/tokenV1.sol --pkg contract --out contract_v1.go
